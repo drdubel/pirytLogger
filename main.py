f2bb34b18cac3503df4341ed2df59a3271f23154
DrDubel <antek.suszko@gmail.com>
2025-07-21 15:24:36 +0200

    webapp for displaying navigation data from last hours

---
+++ main.py
@@ -0,0 +1,36 @@
+import threading
+import psycopg
+
+from flask import Flask, render_template
+
+from save_data import create_tables, data_saver
+from credentials import db_name, db_user
+
+
+db_host = "localhost"
+db_port = "5432"
+
+app = Flask(__name__)
+
+
+def get_data():
+    with psycopg.connect(f"host={db_host} port={db_port} dbname={db_name} user={db_user}") as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM hourly_navigation_summary ORDER BY id DESC;")
+            data = cur.fetchall()
+            colnames = [desc[0] for desc in cur.description]
+        
+    return colnames, data
+
+
+@app.route('/')
+def index():
+    columns, data = get_data()
+    return render_template("index.html", columns=columns, data=data)
+
+
+if __name__ == "__main__":
+    create_tables()
+    threading.Thread(target=data_saver, daemon=True).start()
+
+    app.run(debug=True, threaded=True, use_reloader=False)
